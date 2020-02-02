b8af26404a3ad02b90c1bbdc8cf7a2078d29673a
OpenCarbonWatchAdmin <57277151+OpenCarbonWatchAdmin@users.noreply.github.com>
2020-02-02 10:15:21 +0100

    Weekly assessment statistics for France

---
+++ weekly_best.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sys
+
+# Base file path for data
+
+output_path = sys.argv[1]
+old_path = output_path + sys.argv[2] + '/assessments.csv'
+new_path = output_path + sys.argv[3] + '/assessments.csv'
+
+# Compare old and new assessments
+
+old_assessments = pd.read_csv(old_path, index_col=False, encoding='UTF-8')
+new_assessments = pd.read_csv(new_path, index_col=False, encoding='UTF-8')
+
+old_assessments = old_assessments[old_assessments['is_draft'] == 'Non']
+new_assessments = new_assessments[new_assessments['is_draft'] == 'Non']
+
+new_assessments = new_assessments[~ new_assessments['id'].isin(old_assessments['id'])]
+
+print("Total number of new published assessments: %d." % new_assessments.shape[0])
+
+# Look for high quality new assessments
+
+good = new_assessments
+good = good[good['total_scope_3'] > 0].copy()
+good['total'] = good['total_scope_1'] + good['total_scope_2'] + good['total_scope_3']
+good['reductions'] = good.fillna(0)['reductions_scope_1_2'] + good.fillna(0)['reductions_scope_1'] + good.fillna(0)['reductions_scope_2'] + good.fillna(0)['reductions_scope_3']
+good['ratio'] = good['reductions'] / good['total']
+good = good[good['ratio'] >= 0.05].copy()
+good = good[good['ratio'] <= 0.33].copy()
+
+good.to_csv(output_path + 'good.csv', sep=';', index=False, encoding='Windows-1252')
